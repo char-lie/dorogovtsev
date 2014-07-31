f1038d6cd6e63c63648f0eb4c62a7deff571bb73
Valeriy Krygin <webmaster976@gmail.com>
2014-07-31 22:15:07 +0300

    Added unadded before files

---
+++ pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+    echo "Type the commit comment!" &1>2
+    exit 1
+fi
+git pull https://github.com/char-lie/dorogovtsev
+git add pull.sh
+git add common/*.tex common/*.bib common/*.bst
+git add README.md
+git add PT/*.tex PT/index.pdf PT/part*/*.tex MS/part*/README.md PT/*.latexmain
+git add MS/*.tex MS/index.pdf MS/part*/*.tex MS/part*/README.md MS/*.latexmain
+git add MustKnow/*.tex MustKnow/index.pdf MustKnow/*.latexmain
+git commit -m "$1"
+git push https://char-lie@github.com/char-lie/dorogovtsev

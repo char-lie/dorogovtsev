70d4f191003150ea2a2a04893003e1337f07a9e4
char-lie <webmaster976@gmail.com>
2014-09-02 21:11:36 +0300

    Started Stochastic Processes

--- pull.sh
+++ pull.sh
@@ -10,6 +10,7 @@ git add README.md
 git add PT/*.tex PT/index.pdf PT/part*/*.tex MS/part*/README.md PT/*.latexmain
 git add MS/*.tex MS/index.pdf MS/preface/*.tex MS/part*/*.tex MS/part*/README.md MS/*.latexmain
 git add MS/tikz/*.tex
+git add SP/*.tex SP/index.pdf SP/part*/*.tex SP/*.latexmain
 git add MustKnow/*.tex MustKnow/index.pdf MustKnow/*.latexmain
 git commit -m "$1"
 git push https://char-lie@github.com/char-lie/dorogovtsev
